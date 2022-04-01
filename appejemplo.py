561fd7019dd2da294106a3652875fcb6f604656a
Danisve <102831644+Danisve@users.noreply.github.com>
2022-04-01 14:32:53 -0700

    las siguientes lineas eran para

--- appejemplo.py
+++ appejemplo.py
@@ -3,6 +3,7 @@ st.title("mi primer app")
 st.button("dale click")
 #st.button("otro boton")
 import pandas as pd
-df=pd.read_csv('https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv')
-st.write(df)
-st.map(df)
+#df=pd.read_csv('https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv')
+#st.write(df)
+#st.map(df)
+#las siguientes lineas eran para ver ejemplos

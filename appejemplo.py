4ff4ddd8a4f9b1318e22829c6c5223abe4a65fa0
Danisve <102831644+Danisve@users.noreply.github.com>
2022-04-01 14:52:33 -0700

    f

--- appejemplo.py
+++ appejemplo.py
@@ -4,7 +4,7 @@ click=st.button("dale click")
 st.write("el valor de click es:",click)
 
 if click==true:
-st.image("imagen.jpg")
+st.image("imagen")
 #st.button("otro boton")
 import pandas as pd
 #df=pd.read_csv('https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv')

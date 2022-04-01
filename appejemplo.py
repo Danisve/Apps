f4f353a9c38f08509d2ff7219b88445a8aba75de
Danisve <102831644+Danisve@users.noreply.github.com>
2022-04-01 15:11:19 -0700

    s

--- appejemplo.py
+++ appejemplo.py
@@ -19,5 +19,15 @@ st.markdown("#titulo")
 st.markdown("**este es una viñeta**")
 numl = st.slider('elige un numero 1', 0, 130, 25)
 num2 = st.slider('elige un numero 2', 0, 130, 25)
-suma= num1 + num2
+suma= num1+num2
 st.write ("la suma de",num1,"y",num2,"es:",suma)
+st.write("ahora multipliquemos")
+nn1 = st.number_input("dame n1")
+nn2 = st.number_input("dame n2")
+mult=nn1*nn2
+st.write("la multiplicacion de" ,nn1, "y" ,nn2, "es:", mult)
+with st.sidebar:
+    num1=st.slider('elige el numero1', 0,130,25)
+    num2=st.slider('elige el numero1', 0,130,25)
+    st.write("la suma de los dos es", suma)
+    
